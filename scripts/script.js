c0e408c9594c7e8c575e667987cd001e718fcd96
monaemhossain <monaemhs@gmail.com>
2023-09-02 15:06:43 +0300

    added functionality to sort content by view count

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,10 @@
-const screenWidth = `max-w-screen-xl mx-auto py-5`
+const screenWidth = `xl:max-w-screen-xl mx-auto py-5 px-4`
 const main = document.getElementById('main');
 main.classList = screenWidth;
 
 const header = document.getElementById('header');
 header.classList = screenWidth;
+header.classList.add('border-b-2')
 header.innerHTML = `
 <div class="navbar bg-base-100 hidden md:flex">
     <div class="navbar-start">
@@ -12,27 +13,29 @@ header.innerHTML = `
         </a>
     </div>
     <div class="navbar-center">
-        <a class="btn sort-btn">Sort by view</a>
+        <a id="sort-btn" class="btn sort-btn">Sort by view</a>
     </div>
     <div class="navbar-end">
         <a href="./blog.html" class="btn bg-[#FF1F3D] text-white hover:text-black">Blog</a>
     </div>
-    
-    <div class="md:hidden text-center space-y-3">
-        <div>
-            <a class="btn btn-ghost pl-0 hover:cursor-pointer">
-                <img src="./images/logo.png" alt="pHero Logo">
-            </a>
-        </div>
-        <div>
-            <a class="btn sort-btn">Sort by view</a>
-        </div>
-        <div>
-            <a href="./blog.html" class="btn bg-[#FF1F3D]">Blog</a>
-        </div>
+</div>
+
+<div class="md:hidden grid text-center space-y-3">
+    <div>
+        <a href="./index.html" class="btn btn-ghost pl-0 hover:cursor-pointer">
+            <img src="./images/logo.png" alt="pHero Logo">
+        </a>
+    </div>
+    <div>
+        <a class="btn sort-btn">Sort by view</a>
+    </div>
+    <div>
+        <a href="./blog.html" class="btn bg-[#FF1F3D]">Blog</a>
     </div>
 </div>
+
 `;
+
 
 // pHero data
 const contentCategories = async () => {
@@ -40,7 +43,7 @@ const contentCategories = async () => {
     const data = await response.json();
     const categoryData = data.data;
     categoryMenu(categoryData)
-    pHeroContent('1000', categoryData);
+    pHeroContent(categoryData[0].category_id);
 }
 
 
@@ -50,7 +53,7 @@ const categoryMenu = (category) => {
     menuWrapper.classList = `grid item-center justify-center mb-8`
     const ul = document.createElement('ul');
     // ul.id='menu';
-    ul.classList = `menu menu-horizontal gap-3`;
+    ul.classList = `menu menu-horizontal gap-3 flex-wrap items-center justify-center`;
     menuWrapper.appendChild(ul);
     main.appendChild(menuWrapper)
     category.forEach(item => {
@@ -58,29 +61,51 @@ const categoryMenu = (category) => {
         const categoryId = item.category_id;
         // console.log(categoryName);
         const li = document.createElement('li');
-        li.classList = `bg-base-200 rounded-md hover:bg-[#FF1F3D]`;
-        li.innerHTML = `<a onclick="pHeroContent('${categoryId}')" class='hover:text-white'>${categoryName}</a>`
+        li.classList = `btn bg-base-400 rounded-md hover:bg-[#FF1F3D]`;
+        li.classList.remove('active')
+        li.setAttribute('onclick', 'btnActive(this)');
+        li.innerHTML = `<a onclick="pHeroContent('${categoryId}')" class=''>${categoryName}</a>`
         ul.appendChild(li);
     });
 };
 
-// pHero Contents
-const pHeroContent = async (categoryIdValue, status) => {
-    // console.log(categoryIdValue);
-    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryIdValue}`);
-    const data = await response.json();
-    const contentData = data.data;
-    const dataStatus = data.status;
-    // console.log(data.status);
+const btnActive = (value) => {
+    value.classList.add('active');
+}
+// process of sorting content
+const sortByViews = (data) => {
+    data.sort((a,b) => {
+        const num1 = a.others.views.replace("K", "")*1000;
+        const num2 = b.others.views.replace("K", "")*1000;
+        return num2 - num1;
+        
+    });
+}
 
+const contentCard = (cardContent) => {
     const contentWrapper = document.getElementById('content-wrapper');
-    contentWrapper.classList = '';
     contentWrapper.innerHTML = '';
-
-    if(dataStatus == true){
-        contentWrapper.classList = "grid grid-cols-4 gap-6";
+    
+    if(cardContent == ""){
+        contentWrapper.classList = "no-content";
+        const noContent = document.createElement('div');
+        noContent.classList =  "grid justify-center items-center h-full mt-24";
+        noContent.innerHTML = `
+        <div class="card w-96 text-center">
+            <figure><img src="./images/no-data-icon.png" alt="No content icon" /></figure>
+            <div class="card-body">
+                <h2 class="card-title text-3xl font-bold">
+                    Oops!! Sorry, There is no content here
+                </h2>
+            </div>
+        </div>
+        `;
+        contentWrapper.appendChild(noContent);
+    }
+    else{
+        contentWrapper.classList = "xl:max-w-screen-xl lg:max-w-screen-lg sm:max-w-screen-sm mx-auto grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6 justify-around";
         main.appendChild(contentWrapper);
-        contentData.forEach(content => {
+        cardContent.forEach(content => {
             // card data
             const thumbnail = content.thumbnail;
             const title = content.title;
@@ -90,9 +115,9 @@ const pHeroContent = async (categoryIdValue, status) => {
             const views = content.others.views;
 
             const card = document.createElement('div');
-            card.classList = "card max-w-xs bg-base-100";
+            card.classList = "card max-w-xs mx-auto bg-base-100";
             card.innerHTML = `
-            <figure class="h-52 bg-white rounded-lg"><img src="${thumbnail}" alt="${title}" class="h-full w-full"/></figure>
+            <figure class="md:h-48 h-44 bg-white rounded-lg"><img src="${thumbnail}" alt="${title}" class="min-h-full min-w-full"/></figure>
             <div class="card-body p-5 flex-row items-start justify-start gap-4">
                 <div class='h-10 w-10'>
                     <img src="${profilePicture}" alt="${authorName}" class="h-full w-full rounded-full"/>
@@ -113,22 +138,31 @@ const pHeroContent = async (categoryIdValue, status) => {
             `
             contentWrapper.appendChild(card);
         });
-    }else{
-        contentWrapper.classList = "no-content";
-        const noContent = document.createElement('div');
-        noContent.classList =  "grid justify-center items-center h-full mt-24";
-        noContent.innerHTML = `
-        <div class="card w-96 text-center">
-            <figure><img src="./images/nodataicon.png" alt="No content icon" /></figure>
-            <div class="card-body">
-                <h2 class="card-title text-3xl font-bold">
-                    Oops!! Sorry, There is no content here
-                </h2>
-            </div>
-        </div>
-        `;
-        contentWrapper.appendChild(noContent);
     }
 }
 
+// regenerated card with sorted contents
+const sortContent = (sortedData) => {
+    const contentWrapper = document.getElementById('content-wrapper');
+    contentWrapper.innerHTML = '';
+    contentCard(sortedData);
+}
+
+
+// pHero Contents
+const pHeroContent = async (categoryIdValue) => {
+    // console.log(categoryIdValue);
+
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryIdValue}`);
+    const data = await response.json();
+    const contentData = data.data;
+    const dataStatus = data.status;
+
+    document.getElementById('sort-btn').addEventListener('click', function(){
+        sortByViews(contentData)
+        sortContent(contentData)
+    })
+    contentCard(contentData);
+    
+}
 contentCategories()
